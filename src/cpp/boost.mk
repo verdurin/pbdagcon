28bd05816fd2338bdea093c2ff59ffce1f3955a7
James Drake <jdrake@pacificbiosciences.com>
2015-06-01 12:51:28 -0700

    Fix up test builder
    Solve relative path issue when fetching boost headers.  Reformat recipes
    for consistency.  Need to fix test_target test.

--- src/cpp/boost.mk
+++ src/cpp/boost.mk
@@ -3,12 +3,14 @@ URI := https://www.dropbox.com/s/g22iayi83p5gbbq/boost_1_58_0-headersonly.tbz2\?
 # Obtain the boost URI and extract it into PWD
 GET_BOOST := curl -L $(URI) | tar xjf -
 
+MYPATH__ := $(dir $(CURDIR)/$(word $(words $(MAKEFILE_LIST)),$(MAKEFILE_LIST)))
+
 # Three ways to boost: 1) Internal PBI repot, 2) URI, 3) User specified.
 
 ifdef boost
 BOOST_HEADERS := $(boost)
 else
-BOOST_HEADERS := third-party/boost_1_58_0-headersonly
+BOOST_HEADERS := $(MYPATH__)third-party/boost_1_58_0-headersonly
 endif
 
 ifneq ($(wildcard $(PREBUILT)/boost/boost_1_58_0-headersonly/*),)
