074c0b7ed1ba294e33be9f60b14a88a36b379299
Mengjuei Hsieh <mhsieh@pacificbiosciences.com>
2018-02-27 17:13:49 -0800

    storing inline script for pbdagcon

---
+++ bamboo_test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+export PATH=$PWD/deployment/bin:$PATH
+export LD_LIBRARY_PATH=$PWD/deployment/bin:$LD_LIBRARY_PATH
+type module >& /dev/null \
+|| . /mnt/software/Modules/current/init/bash
+module load gcc
+module load htslib
+module load hdf5-tools
+module load zlib
+
+make -C repos/pbdagcon/build check
+chmod +w -R repos/pbdagcon
